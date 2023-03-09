6f44da31fc9097c25dca901bae38f8295ca012da
Theofanis <theofanis.tsakiris@physics.uu.se>
2023-03-09 13:28:03 +0100

    added function for visualization + improv file reading

--- functionsForOpticalData.py
+++ functionsForOpticalData.py
@@ -35,14 +35,16 @@ def TryReadFirst():
     numberOfPointsInSpectrum=0
     
     WavelegthsRef,IntensitiesRef=[],[]
-    file = open(txtFileList[0], 'rt')
-    for myline in file:
-        numberOfPointsInSpectrum+=1
-        #txt has a specific format with 2 columns seperated by ;
-        wavelegth,intensity=myline.rstrip('\n').split(";")
-        WavelegthsRef.append( float(wavelegth))
-        IntensitiesRef.append( float(intensity))
-    file.close()
+    try:
+        file = open(txtFileList[0], 'rt')
+        for myline in file:
+            numberOfPointsInSpectrum+=1
+            #txt has a specific format with 2 columns seperated by ;
+            wavelegth,intensity=myline.rstrip('\n').split(";")
+            WavelegthsRef.append( float(wavelegth))
+            IntensitiesRef.append( float(intensity))
+    finally:
+        file.close()
     
     WavelegthsRef = np.array(WavelegthsRef)
     IntensitiesRef=np.array(IntensitiesRef)
@@ -50,29 +52,32 @@ def TryReadFirst():
 
 numberOfPointsInSpectrum,WavelegthsRef,IntensitiesRef = TryReadFirst()
 
+#Visualize the first Spectrum
+def VisualizeFirst():
+    fig,ax=plt.subplots()
+    ax.set_xlabel("Wavelength (nm)");ax.set_ylabel('Intensity')
+    plt.plot(WavelegthsRef,IntensitiesRef,ms=0.5)
+
+VisualizeFirst()    
+
 #----Extract all intensity data----
 def ScanFiles(): 
     arrInt = np.zeros( (numberOfSpectra,numberOfPointsInSpectrum),dtype=np.float64)
     specIndex=0
     for txtFile in txtFileList: #For Each Spectrum
         arrIndex=0
-        file = open(txtFile, 'rt')
-        for line in file:  
-            #extract the data
-            wavelength,intensity=line.rstrip('\n').split(";")
-            arrInt[specIndex,arrIndex]= intensity
-            arrIndex+=1
-        #end for
-        specIndex+=1
-    #end for
+        try:
+            file = open(txtFile, 'rt')
+            for line in file:  
+                #extract the data
+                wavelength,intensity=line.rstrip('\n').split(";")
+                arrInt[specIndex,arrIndex]= intensity
+                arrIndex+=1
+            #end for (lines)
+            specIndex+=1
+        finally:
+            file.close()   
+    #end for (files)
     return  arrInt 
             
 arrInt  =   ScanFiles()
-
-#checking some data to verify that everything was read correctly
-print(arrInt);print()
-print(arrInt[0,2300:2305]);print()
-print(arrInt[:,0])
-            
-            
-            
