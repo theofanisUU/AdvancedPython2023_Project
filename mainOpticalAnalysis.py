c5a7b217308921c538f6b4fb8a3a5ab359f9fd90
Theofanis <theofanis.tsakiris@physics.uu.se>
2023-03-10 12:57:11 +0100

    more independent function, moved function calls and input to main

--- mainOpticalAnalysis.py
+++ mainOpticalAnalysis.py
@@ -1,5 +1,17 @@
-# import functionsForOpticalData
+import functionsForOpticalData as func
 
 #----Input----
+integrStartWav=630
+integrFinishWav=1000
+
 
 #----Functionality----
+numberOfSpectra,txtFileList = func.locateData()    
+launchTime,numberOfPointsInSpectrum,Wavelengths,IntensitiesRef = \
+    func.TryReadFirstDataset(txtFileList)
+timesFromLaunchInHours,intensities,averageIntensities = \
+    func.ScanFiles(txtFileList,numberOfSpectra,numberOfPointsInSpectrum,launchTime,Wavelengths,integrStartWav,integrFinishWav)
+
+func.TryReadFirstDataset(txtFileList)
+func.plotAverageOverTime(timesFromLaunchInHours,averageIntensities)
+print("allok") 
